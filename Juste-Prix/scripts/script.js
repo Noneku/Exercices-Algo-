c98cce75ced3d8858ac4cdccffcf989ff7f61498
None.ku <dev.nassim.pro@gmail.com>
2023-03-03 09:39:06 +0100

    update

--- Juste-Prix/scripts/script.js
+++ Juste-Prix/scripts/script.js
@@ -1,29 +1,58 @@
-let Price = window.prompt("Entrer le prix");
-let Limite = 9;
+// let Price = window.prompt("Entrer le prix");
+// let Limite = 9;
 
-if (Price) {
-    console.log("Le jeux peux commencer !");
+// if (Price) {
+//     console.log("Le jeux peux commencer !");
 
-    for (let limite = 0; limite < 10; limite++) {
-        let Game = window.prompt("Entrer une valeur, vite !");
+//     for (let limite = 0; limite < 10; limite++) {
+//         let Game = window.prompt("Entrer une valeur, vite !");
 
-        if(Game != Price) {
-                    if(Game < Price){
-                        console.log("C'est plus !");
-                    }
-                    else {
-                        console.log("C'est moin !");
-                    } 
-                    console.log(`Il vous reste ${Limite--} tentative(s), vous avez perdu`);        
-                }
-                else{
-                    console.log("Vous avez gagné !");
-                }
-    }
-    //Je ferme le jeux
-    Price = false;
-}
+//         if(Game != Price) {
+//                     if(Game < Price){
+//                         console.log("C'est plus !");
+//                     }
+//                     else {
+//                         console.log("C'est moin !");
+//                     } 
+//                     console.log(`Il vous reste ${Limite--} tentative(s), vous avez perdu`);        
+//                 }
+//                 else{
+//                     console.log("Vous avez gagné !");
+//                 }
+//     }
+//     //Je ferme le jeux
+//     Price = false;
+// }
 
-if(Limite = 0){
-    console.log("Vous avez perdu ! ");
+// if(Limite = 0){
+//     console.log("Vous avez perdu ! ");
+// }
+
+let rdm = Math.floor(Math.random() * 100 + 1);
+console.log(rdm);
+let sommeUtilisateur;
+let cpt = 1;
+let message ="perdu";
+let egale, plusGrand, plusPetit;
+egale=cpt<11 && sommeUtilisateur === rdm;
+plusGrand=sommeUtilisateur>rdm && cpt<11;
+plusPetit=sommeUtilisateur<rdm  && cpt<11;
+
+function getPrix(){
+    sommeUtilisateur = document.getElementById("prix").value;  
+    
+    if (egale){
+        message="Bravo vous avez trouvez " + cpt;
+     
+    } else if( plusGrand){
+          message="trop élevé " + cpt;
+           
+        }  else if (plusPetit){
+            message="trop bas " + cpt;
+        }
+        else {
+            message="perdu" + cpt;
+        }
+        cpt++;
+        console.log(message);
 }
--- Trie-A-Bulle/scripts/script.js
+++ Trie-A-Bulle/scripts/script.js
@@ -1,12 +1,27 @@
-let Content = [10,20,5,30,40,13];
 
-for (let i = 0; i < Content.length; i++) {
+let T = [8,2,9,4,1,7,3];
+let N = T.length;
+
+let element;
+
+let cpt = 0;
+let cptBoucle1 = 0;
+let cptBoucle2 = 0;
+
+function TriBulles(){
+
+
+   for (let i = N-1; i > 0; i--){
+    cptBoucle1++
     for (let j = 0; j < i; j++) {
-        if(Content[j] > Content[j+1]){
-            Content[j+1] = Content[j];
+        cptBoucle2++;
+        if ( T[j+1] < T[j]) {
+            element = T[j+1];
+            T[j+1] = T[j]
+            T[j] = element
+            cpt+=3
+            console.log(T)
         }
-        
+
+
     }
-     
-    console.log(Content);
-} 
